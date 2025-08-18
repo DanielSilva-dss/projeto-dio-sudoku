f0b55023efdde407046c3cf83fd162023146e9b7
DanielSilva-dss <danielss.ds50@gmail.com>
2025-08-17 23:30:08 -0300

    Fim do Projeto

--- src/br/com/pessoal/ui/custom/input/NumberText.java
+++ src/br/com/pessoal/ui/custom/input/NumberText.java
@@ -21,7 +21,7 @@ public class NumberText extends JTextField implements EventListener {
         this.setFont(new Font("Arial", Font.PLAIN, 20));
         this.setHorizontalAlignment(CENTER);
         this.setDocument(new NumberTextLimit());
-        this.setEnabled(space.isFixed());
+        this.setEnabled(!space.isFixed());
 
         if (space.isFixed()) {
             this.setText(space.getActual().toString());
